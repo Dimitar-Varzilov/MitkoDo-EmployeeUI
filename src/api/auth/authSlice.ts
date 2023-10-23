66b91f1f47e8ca83db2fe56401a14eaa81db9814
Dimitar Varzilov <dnvarzilov@diadraw.com>
2023-10-23 11:44:26 +0300

    add logged state

--- src/api/auth/authSlice.ts
+++ src/api/auth/authSlice.ts
@@ -2,15 +2,17 @@ import { createSlice } from '@reduxjs/toolkit'
 
 import type { IUser } from '../../interfaces'
 import { type RootState } from '../../store'
-import { getToken } from '../../utilities'
+import { getToken, removeToken } from '../../utilities'
 import { ReducerNames } from '../types'
 
 interface AuthState {
+  isLogged: boolean
   token: string | null
   user?: IUser
 }
 
 const initialState: AuthState = {
+  isLogged: !!getToken(),
   token: getToken(),
   user: undefined,
 }
@@ -19,8 +21,14 @@ export const authSlice = createSlice({
   name: ReducerNames.Auth,
   initialState: initialState,
   reducers: {
+    loggedIn: (state) => {
+      state.isLogged = true
+    },
     logOut: (state) => {
+      state.isLogged = false
       state.user = undefined
+      state.token = null
+      removeToken()
     },
     setUser: (state, action: { payload: IUser }) => {
       state.user = action.payload
@@ -32,5 +40,7 @@ export const authSlice = createSlice({
 })
 
 export const selectUser = (state: RootState) => state.auth.user
-export const { logOut, setUser, tokenReceived } = authSlice.actions
+export const selectToken = (state: RootState) => state.auth.token
+export const selectIsLogged = (state: RootState) => state.auth.isLogged
+export const { loggedIn, logOut, setUser, tokenReceived } = authSlice.actions
 export default authSlice.reducer
--- src/pages/Wrapper.tsx
+++ src/pages/Wrapper.tsx
@@ -1,11 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, type FC } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 
+import { logOut } from '../api/auth/authSlice'
 import Spinner from '../components/Spinner'
 import { useAppContext } from '../context'
+import { useAppDispatch, useAppSelector } from '../store'
 
 const Wrapper: FC = () => {
-  const { error, isLoading, isLoggedIn, logOut } = useAppContext()
+  const { error, isLoading } = useAppContext()
+  const { isLogged } = useAppSelector((state) => state.auth)
+  const dispatch = useAppDispatch()
   const navigate = useNavigate()
 
   const handleGoHome = () => {
@@ -13,21 +18,21 @@ const Wrapper: FC = () => {
   }
 
   const handleLogOut = () => {
-    logOut()
+    dispatch(logOut())
     navigate('/')
   }
 
-  useEffect(() => {
-    if (!isLoggedIn) {
-      navigate('/')
-    }
-  }, [isLoggedIn])
+  // useEffect(() => {
+  //   if (!isLogged) {
+  //     navigate('/')
+  //   }
+  // }, [isLogged])
 
   if (isLoading) return <Spinner />
   if (error) return <p>{error.title}</p>
   return (
     <>
-      {isLoggedIn && (
+      {isLogged && (
         <>
           <button onClick={handleGoHome}>Main</button>
           <button onClick={handleLogOut}>Log out</button>
